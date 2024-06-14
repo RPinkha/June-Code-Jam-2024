cf35a7178f1abd385bee1d6d5db7a6af4d23398b
RuvenPinkhasov <rubi00010@gmail.com>
2024-06-14 01:45:58 -0400

    feat: added animation to the button and made it open the navbar

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 export const footerYear = document.querySelector(".footer__year");
+export const navButton = document.querySelector(".nav__toggle");
+export const navCloseButton = document.querySelector(".nav__close");
+export const navButtonTopLine = document.querySelector(".nav__toggle-line_top");
+export const navButtonBottomLine = document.querySelector(
+  ".nav__toggle-line_bottom"
+);
+export const navLinks = document.querySelector(".nav__links");
+
 export const currentYear = new Date().getFullYear();
