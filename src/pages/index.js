f1a2614cf0e183a6de8ac6cdb0c96d5ca9eec215
ChristianTo <christian.to@outlook.com>
2024-06-14 12:53:17 -0500

    form and about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,19 @@ import "./index.css";
 import { footerYear, currentYear } from "../utils/constants.js";
 
 footerYear.textContent = currentYear;
+
+import "../scripts/validate.js";
+
+//suggest submit button
+const thankYou = document.querySelector(".suggest__thankyou");
+function removeThankYou() {
+  thankYou.classList.remove("suggest__thankyou_show");
+}
+const submitSuggestion = function () {
+  thankYou.classList.add("suggest__thankyou_show");
+  setTimeout(removeThankYou, 2000);
+};
+document.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  submitSuggestion();
+});
