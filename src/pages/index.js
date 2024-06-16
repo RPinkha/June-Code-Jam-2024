14681ae72b2d2bce535bed171203219859e2521d
RuvenPinkhasov <rubi00010@gmail.com>
2024-06-16 01:36:07 -0400

    feat: added map to the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,3 +34,29 @@ document.addEventListener("submit", (evt) => {
   evt.preventDefault();
   submitSuggestion();
 });
+
+const map = L.map("map").setView([39.8283, -98.5795], 4);
+
+const redCircleIcon = L.divIcon({
+  className: "custom-marker-icon",
+  html: '<div style="background-color: red; width: 12px; height: 12px; border-radius: 50%;"></div>',
+});
+
+// Add a tile layer to the map (OpenStreetMap tiles)
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap contributors",
+}).addTo(map);
+
+// Add markers and collect the coordinates
+const parkCoordinates = orderedParks.map((park) => {
+  L.marker([park.latitude, park.longitude], { icon: redCircleIcon })
+    .addTo(map)
+    .bindPopup(`<b>${park.name}</b>`);
+  return [park.latitude, park.longitude];
+});
+
+// Draw lines between the parks
+const polyline = L.polyline(parkCoordinates, { color: "blue" }).addTo(map);
+
+// Fit the map to the polyline bounds
+map.fitBounds(polyline.getBounds());
